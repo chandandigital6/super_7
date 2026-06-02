c56dbc5e4da774477c2eaab2392f275758d2f02f
chandan <chandandigital6@gmail.com>
2026-06-02 23:51:56 +0530

    changes

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -3,13 +3,13 @@
 ])
 
 @section('content')
-    <section class="bg-white">
+    {{-- <section class="bg-white">
         <div class="text-center py-4">
             <h2 class="text-2xl font-bold uppercase">
                 Super A7 Satta King Result Today – Updated Live Instantly
             </h2>
         </div>
-    </section>
+    </section> --}}
 
 
    <style>
