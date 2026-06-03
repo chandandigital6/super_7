8944718c47678c12c620b05c911fdd379b6ffde6
chandandigital6 <chandandigital6@gmail.com>
2026-06-03 12:50:53 +0530

    dard romove

--- resources/views/pages/settings/⚡appearance.blade.php
+++ resources/views/pages/settings/⚡appearance.blade.php
@@ -7,7 +7,7 @@ new #[Title('Appearance settings')] class extends Component {
     //
 }; ?>
 
-<section class="w-full">
+{{-- <section class="w-full">
     @include('partials.settings-heading')
 
     <flux:heading class="sr-only">{{ __('Appearance settings') }}</flux:heading>
@@ -19,4 +19,41 @@ new #[Title('Appearance settings')] class extends Component {
             <flux:radio value="system" icon="computer-desktop">{{ __('System') }}</flux:radio>
         </flux:radio.group>
     </x-pages::settings.layout>
+</section> --}}
+
+
+
+
+
+
+
+
+<section class="w-full">
+    @include('partials.settings-heading')
+
+    <flux:heading class="sr-only">{{ __('Appearance settings') }}</flux:heading>
+
+    <x-pages::settings.layout
+        :heading="__('Appearance')"
+        :subheading="__('Update the appearance settings for your account')"
+    >
+        <flux:radio.group
+            x-data
+            x-init="$flux.appearance = 'light'"
+            variant="segmented"
+            x-model="$flux.appearance"
+        >
+            <flux:radio value="light" icon="sun">{{ __('Light') }}</flux:radio>
+            <flux:radio value="dark" icon="moon">{{ __('Dark') }}</flux:radio>
+            <flux:radio value="system" icon="computer-desktop">{{ __('System') }}</flux:radio>
+        </flux:radio.group>
+    </x-pages::settings.layout>
 </section>
+
+<script>
+    localStorage.setItem('flux.appearance', 'light');
+    document.documentElement.classList.remove('dark');
+</script>
+
+
+
